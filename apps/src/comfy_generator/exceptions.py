2883469ba5b307ef225a799df31705e8fe612f7a
Vito Leonelli <leonellivito444@gmail.com>
2026-06-19 22:38:15 -0300

    Implemented basic instance atributes + added few typehint features

--- apps/src/comfy_generator/exceptions.py
+++ apps/src/comfy_generator/exceptions.py
@@ -9,3 +9,11 @@ class NonSettableInstanceException(Exception):
 class AssetsPathNotFoundError(Exception):
     def __init__(self, error):
         super().__init__(error)
+
+class RootProjectFolderNotFoundError(Exception):
+    def __init__(self, error):
+        super().__init__(error)
+
+class IllegalRootProjectAlteration(Exception):
+    def __init__(self, error):
+        super().__init__(error)
--- apps/src/comfy_generator/file_system.py
+++ apps/src/comfy_generator/file_system.py
@@ -9,21 +9,30 @@ This file must isolate all file interactions, handling:
 import json
 import sys
 from pathlib import Path
+from typing import Final, NoReturn, Never
 
 from exceptions import *
 
 class FileSystem:
 
     # Class Attributes:
-    ALLOWED_PLATFORMS = ("win32", "darwin", "linux")
+    ALLOWED_PLATFORMS: Final[tuple[str, str, str]] = ("win32", "darwin", "linux")
     
     def __init__(self):
         current_system: str = sys.platform
-        if current_system not in self.ALLOWED_PLATFORMS:
+
+        if not any(current_system.startswith(platform) for platform in self.ALLOWED_PLATFORMS):
             raise InvalidOperatingSystem(f"Only PC operating systems are allowed. Not '{current_system}'")
         self.__system: str = current_system
 
-        self.__path_to_assets: Path = Path(__file__).resolve().parents[3] / "assets" / "generated_images"
+        # Store the path to the root of the project
+        self.__project_root: Path = Path(__file__).resolve().parents[3]
+        
+        # Current Path of Script -> cleans up path by starting from the root of the hard drive -> goes 4 levels upward -> overrides 'pathlib' to join the other paths
+        self.__path_to_assets: Path = self.__project_root / "assets" / "generated_images"
+
+        # If the path to the assets and generated_images folder does not exist, it'll make it automatically
+        self.__path_to_assets.mkdir(parents=True, exist_ok=True)
 
     # Getter, setter and deleter methods:
     
@@ -32,8 +41,8 @@ class FileSystem:
         return self.__system
     
     @system.setter
-    def system(self, new_system: str):
-        raise NonSettableInstanceException("Altering operating systems are not permitted")
+    def system(self, new_system: str) -> Never:
+        raise NonSettableInstanceException(f"Altering operating systems are not permitted. Given argument: {new_system}")
     
     @property
     def path_to_assets(self) -> Path:
@@ -44,4 +53,16 @@ class FileSystem:
         if "assets" not in new_path.parts:
             raise AssetsPathNotFoundError(f"The given path does not include the 'assets' folder")
 
+        if not new_path.is_relative_to(self.__project_root):
+            raise RootProjectFolderNotFoundError("The given path is not related to the project root folder")
+
         self.__path_to_assets = new_path
+        self.__path_to_assets.mkdir(parents=True, exist_ok=True)
+
+    @property
+    def project_root(self) -> Path:
+        return self.__project_root
+    
+    @project_root.setter
+    def project_root(self, new_path: Path) -> Never:
+        raise IllegalRootProjectAlteration(f"Altering root project paths is forbidden. Given argument: {new_path}")
